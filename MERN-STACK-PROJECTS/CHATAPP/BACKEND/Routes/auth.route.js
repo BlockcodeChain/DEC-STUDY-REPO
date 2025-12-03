d59900781f0efe2b59b89abad659ba8fcb83b295
BlockcodeChain <sanayasahu2025@gmail.com>
2025-12-04 00:36:26 +0530

    DAY-3

---
+++ MERN-STACK-PROJECTS/CHATAPP/BACKEND/Routes/auth.route.js
@@ -0,0 +1,5 @@
+import express from 'express'
+const router=express.Router();
+router.get('/login',(req,res)=>{
+    res.send("login route")
+})
--- MERN-STACK-PROJECTS/CHATAPP/BACKEND/server.js
+++ MERN-STACK-PROJECTS/CHATAPP/BACKEND/server.js
@@ -9,6 +9,10 @@ app.get('/',(req,res)=>{
     res.status(200).json({message:"server running"})
    
 })
+// AUTHENTICATION
+// 
+// middlewares
+app.use('/api/auth',authRoutes)
 app.listen(Myport,(req,res)=>{
 console.log(`Server running at port ${Myport}`)
 })
